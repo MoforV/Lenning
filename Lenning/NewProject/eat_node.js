fdc9fd069ca51ff43c25fe5f565212d3dd5b7b48
MoforV <2692131262@qq.com>
2024-10-10 09:53:37 +0800

    代码整理了一下

--- Lenning/NewProject/eat_node.js
+++ Lenning/NewProject/eat_node.js
@@ -3,12 +3,6 @@ const app = express();
 const mysql = require('mysql');
 const router = require('./eat_router')
 
-// 数据库查询：select 
-// 	AES_DECRYPT(coffce_name, '123') AS '芳名', 
-// 	AES_DECRYPT(coffce_price, '123') AS '价格', 
-// 	AES_DECRYPT(coffce_image, '123') AS '实拍' 
-// 	FROM coffce_inif;
-
 const db = mysql.createPool({
 	host: '192.168.40.33',
 	user: 'root',
@@ -17,10 +11,6 @@ const db = mysql.createPool({
 })
 
 app.use('/coffce', router)
-// db.query(`select id '索引',  AES_DECRYPT(coffce_name, '123') AS '芳名', AES_DECRYPT(coffce_price, '123') AS '价格', AES_DECRYPT(coffce_image, '123') AS '实拍' FROM coffce_inif;`, (err, rest) => {
-// 	if(err) return console.log(err.message)
-// 	console.log(rest);
-// })
 
 app.listen(9000, ()=>{
 	console.log('Server is running for http://127.0.0.1:9000');
--- Lenning/NewProject/eat_router.js
+++ Lenning/NewProject/eat_router.js
@@ -13,17 +13,25 @@ const db = mysql.createPool({
 app.use(express.static('./page'));
 
 router.get('/', (req, res) => {
-	const query = req.query;
-	let dbsql = `select 
-	AES_DECRYPT(coffce_name, '123') AS 'name', 
-	AES_DECRYPT(coffce_price, '123') AS 'price', 
-	AES_DECRYPT(coffce_image, '123') AS 'Loca' 
-	FROM coffce_inif WHERE id = ${query.id};`
-	// 调用res.send()方法向客户端相应处理结果
-	db.query(dbsql, [], (err, rest, fields) => {
-		if(err) { return console.log(err.message) }
-		res.send("您好 您点的是：" + rest[0].name + "\r\n价格在这边：￥" + rest[0].price + "\r\n图片的话，请移目这边：" + rest[0].Loca);
-	});
+	if(req.query) {
+		const query = req.query;
+		let dbsql = `select 
+		AES_DECRYPT(coffce_name, '123') AS 'name', 
+		AES_DECRYPT(coffce_price, '123') AS 'price', 
+		AES_DECRYPT(coffce_image, '123') AS 'Loca' 
+		FROM coffce_inif WHERE id = ${query.id};`
+		db.query(dbsql, [], (err, rest, fields) => {
+			if(err) { return console.log(err.message) }
+			res.send("您好 您点的是：" +
+			rest[0].name +
+			"\r\n价格在这边：￥" +
+			rest[0].price + "\r\n图片的话，请移目这边：" +
+			rest[0].Loca);
+		});
+	}
+	else{
+		res.send("请在url的最后加上id={您想查询的咖啡}；例如：http://127.0.0.1:9000/?id=1")
+	}
 })
 
 module.exports = router
