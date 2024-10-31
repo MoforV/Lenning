5e4097b5c1eaee83fc6f37965b9f42fd28c2d57e
MoforV <2692131262@qq.com>
2024-10-31 14:48:04 +0800

    更新了一个项目拆解和分析的md文件，里面是我对项目的进一步拆解和分析

--- Lenning/XueXi/db/db_config.js
+++ Lenning/XueXi/db/db_config.js
@@ -1,8 +1,8 @@
 //配置链接数据库参数
 module.exports = {
-    host : 'localhost',
+    host : '192.168.40.33',
     port : 3306,//端口号
     database : 'bookstore',//数据库名
     user : 'root',//数据库用户名
-    password : 'root'//数据库密码
+    password : 'asdasd'//数据库密码
 }
