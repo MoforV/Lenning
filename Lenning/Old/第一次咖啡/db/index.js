8b24bf90f2d1dc882505dee075b842621214193c
MoforV <2692131262@qq.com>
2024-11-04 16:31:54 +0800

    已经改正项目，寻找了三个比较满意的工程环境，项目策划书收尾

--- Lenning/Old/第一次咖啡/db/index.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const db = mysql.createPool({
-	host: '127.0.0.1',
-	user: 'root',
-	password: 'asdasd',
-	database: 'Coffces'
-})
-
-module.exports = db
--- Lenning/Old/第一次咖啡/function/app_cc.js
+++
@@ -1,9 +0,0 @@
-export.cc = (req, res, next) => {
-	res.cc = (err,status = 1) {
-		res.send({
-			status,
-			message: err instanceof Error ? err.message : err,
-		})
-		next()
-	}
-}
